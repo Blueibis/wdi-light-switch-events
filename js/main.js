c2368c245db93d6f81455b3ade0cd916fe5ecb0d
Blueibis <jennie.li.2011@gmail.com>
2017-07-06 14:37:24 -0400

    complete light switch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
 
+var lightSwitch = document.querySelector('.switch');
 
+lightSwitch.addEventListener('click', function(e) {
+
+    switch (this.className) {
+      case 'switch on':
+        return this.className = 'switch off';
+        break;
+      case 'switch off':
+        return this.className = 'switch on';
+      default:
+    }
+})
+
+var background = document.querySelector('body');
+var text = document.querySelector('.status');
+
+  background.addEventListener('click', function(e) {
+
+    switch (this.className) {
+      case 'light':
+        text.innerHTML = 'Hey, who turned off the lights?'
+        return this.className = 'dark';
+        break;
+      case 'dark':
+        text.innerHTML = "It's so bright in here!"
+        return this.className = 'light';
+      default:
+    }
+})
+
+});
